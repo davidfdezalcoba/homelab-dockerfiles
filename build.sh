41183e495c9a76cec965399ae197cd2c46764bc0
David Fernandez Alcoba <davidfdezalcoba@gmail.com>
2024-11-20 23:19:44 +0100

    Add version args for buils

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+APP=$1
+VERSION=$2
+
+set -e 
+set -o pipefail
+
+if [[ -z ${APP} ]]; then
+    echo 'No app with that name, exiting'
+    exit 1
+fi
+
+if [[ -z ${VERSION} ]]; then
+    docker buildx build -t davidfdezalcoba/${APP}:latest ./${APP}
+    docker push davidfdezalcoba/${APP}:latest
+else
+    ARG="$(echo ${APP} | tr '[:lower:]' '[:upper:]')_VERSION"
+    docker buildx build --build-arg ${ARG}=${VERSION} -t davidfdezalcoba/${APP}:${VERSION} ./{APP}
+    docker push davidfdezalcoba/${APP}:${VERSION}
+fi
